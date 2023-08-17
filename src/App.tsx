69527794d6e37cddd7a5481831099e20aba93d16
bvv-1 <fhinnoigsr@gmail.com>
2023-08-18 00:29:30 +0900

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,28 @@
-import './App.css';
+import "./App.css"
 
 /* Components */
-import SongList from './components/SongList/SongList';
+import SongList from "./components/SongList/SongList"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="tempo">
-          tempo
-        </div>
-        <div className="energy">
-          energy
-        </div>
-        <div className="mode">
-          mode
-        </div>
-        <div className="speech">
-          speech
-        </div>
-        <div className="valence">
-          valence
-        </div>
-        <div className="howToUse">
-          how to use
-        </div>
-        <div className="push">
-          push
-        </div>
+        <div className="tempo">tempo</div>
+        <div className="energy">energy</div>
+        <div className="mode">mode</div>
+        <div className="speech">speech</div>
+        <div className="valence">valence</div>
+        <div className="howToUse">how to use</div>
+        <div className="push">push</div>
         <div className="playlist">
           <h1>playlist</h1>
-          <SongList
-          order = {'01'}
-          name = {'アイドル'}
-          artist = {'YOASOBI'}
-          time = {0}
-          />
-          <SongList
-          order = {'02'}
-          name = {'青のすみか'}
-          artist = {'キタニタツヤ'}
-          time = {0}
-          />
-          <SongList
-          order = {'03'}
-          name = {'怪獣の花唄'}
-          artist = {'Vaundy'}
-          time = {0}
-          />
+          <SongList order={"01"} name={"アイドル"} artist={"YOASOBI"} time={0} />
+          <SongList order={"02"} name={"青のすみか"} artist={"キタニタツヤ"} time={0} />
+          <SongList order={"03"} name={"怪獣の花唄"} artist={"Vaundy"} time={0} />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/SongList/SongList.tsx
+++ src/components/SongList/SongList.tsx
@@ -1,13 +1,15 @@
-import styles from './SongList.module.css';
+import styles from "./SongList.module.css"
 
-export default function SongList(props: { order: string, name: string, artist: string, time: number }) {
-    return (
-        <div className={styles.songList}>
-            <div className={styles.container}>
-                <div className={styles.order}>{props.order}</div>
-                <div className={styles.name}>{props.name}</div>
-                <div className={styles.artistTime}>{props.artist}・{props.time}</div>
-            </div>
+export default function SongList(props: { order: string; name: string; artist: string; time: number }) {
+  return (
+    <div className={styles.songList}>
+      <div className={styles.container}>
+        <div className={styles.order}>{props.order}</div>
+        <div className={styles.name}>{props.name}</div>
+        <div className={styles.artistTime}>
+          {props.artist}・{props.time}
         </div>
-    );
+      </div>
+    </div>
+  )
 }
