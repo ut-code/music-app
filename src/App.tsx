022a0e5c8356e91fad948f502bc1b249b569b9ab
Futaba-rgb <obo-futaba742@g.ecc.u-tokyo.ac.jp>
2023-08-17 21:04:11 +0900

    SongList Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+/* Components */
+import SongList from './components/songList';
+
 function App() {
   return (
     <div className="App">
@@ -27,7 +30,25 @@ function App() {
           push
         </div>
         <div className="playlist">
-          playlist
+          <h1>playlist</h1>
+          <SongList
+          order = {'01'}
+          name = {'アイドル'}
+          artist = {'YOASOBI'}
+          time = {0}
+          />
+          <SongList
+          order = {'02'}
+          name = {'青のすみか'}
+          artist = {'キタニタツヤ'}
+          time = {0}
+          />
+          <SongList
+          order = {'03'}
+          name = {'怪獣の花唄'}
+          artist = {'Vaundy'}
+          time = {0}
+          />
         </div>
       </div>
     </div>
