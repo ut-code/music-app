45f3112c06ff475a0fd11fa86bbc6592fd8e5624
bvv-1 <fhinnoigsr@gmail.com>
2023-08-18 20:34:06 +0900

    Add p h1

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -13,8 +13,10 @@ interface Props {
 export default function Slider(props: Props): JSX.Element {
   return (
     <>
+      <div style={{ display: "flex", alignItems: "center", gap: "12px" }}>
       <h3>{props.name}</h3>
-      <div className={"slider-description"}>{props.description}</div>
+      <p>{props.description}</p>
+      </div>
       <RangeSlider
         value={props.value}
         onChange={(changeEvent) => props.onChange(+changeEvent.target.value)}
