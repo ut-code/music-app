73d431dce24dfa94991d34d3854dd8835f32f264
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-08-19 14:18:03 +0900

    Remove unnecessary codes

--- frontend/src/components/SongList/SongList.tsx
+++ frontend/src/components/SongList/SongList.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react"
 import styles from "./SongList.module.css"
-import Music from "../../api/musicType"
-import { getAllMusic } from "../../api/getAllMusic"
 
 export type SongData = {
   order: string
@@ -11,24 +8,13 @@ export type SongData = {
 }
 
 export default function SongList(props: SongData) {
-  const [musicData, setMusicData] = useState<Music[]>([])
-
-  useEffect(() => {
-    async function tmp(){
-      return await getAllMusic()
-    }
-    tmp().then((music)=>{
-      setMusicData(music)
-    })
-  }, [])
-
   return (
     <div className={styles.songList}>
       <div className={styles.container}>
         <div className={styles.order}>{props.order}</div>
         <div className={styles.name}>{props.name}</div>
         <div className={styles.artistTime}>
-          {props.url}・{props.time}・{JSON.stringify(musicData)}
+          {props.url}・{props.time}
         </div>
       </div>
     </div>
